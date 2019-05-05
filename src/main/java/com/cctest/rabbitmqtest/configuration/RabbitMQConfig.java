d4d1b56b0a32c743a4e7aa0b72261e37b29d909d
chengcheng1 <chengcheng1@yidian-inc.com>
2019-05-05 18:06:03 +0800

    learning

--- src/main/java/com/cctest/rabbitmqtest/configuration/RabbitMQConfig.java
+++ src/main/java/com/cctest/rabbitmqtest/configuration/RabbitMQConfig.java
@@ -1,8 +1,13 @@
 package com.cctest.rabbitmqtest.configuration;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -28,4 +33,37 @@ public class RabbitMQConfig {
 
         return rabbitAdmin;
     }
+
+    @Bean
+    public TopicExchange exchange001(){
+        return new TopicExchange("topic001", true,false);
+    }
+
+    @Bean
+    public Queue queue001(){
+        return new Queue("queue001", true);
+    }
+
+    @Bean
+    public Queue queue_image(){
+        return new Queue("image_queue", true);
+    }
+
+    @Bean
+    public Queue queue_pdf(){
+        return new Queue("pdf_queue", true);
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public Binding binding001(){
+        return BindingBuilder.bind(queue001()).to(exchange001()).with("spring.topic001");
+    }
+
+
 }
--- src/test/java/com/cctest/rabbitmqtest/testRabbit/RabbitAdminTest.java
+++ src/test/java/com/cctest/rabbitmqtest/testRabbit/RabbitAdminTest.java
@@ -2,14 +2,14 @@ package com.cctest.rabbitmqtest.testRabbit;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.Exchange;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.HashMap;
 
 @SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -18,11 +18,32 @@ public class RabbitAdminTest {
     @Autowired
     private RabbitAdmin rabbitAdmin;
 
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
     @Test
     public void testRabbitAdimin(){
-        Exchange exchange = new TopicExchange("spring_topic_exchange");
-        Queue queue = new Queue("spring_topic_queue");
+        Exchange exchange = new TopicExchange("spring_topic_exchange", false, false);
+        FanoutExchange fanoutexchange = new FanoutExchange("spring_fanout_exchange", false, false);
+        Queue queue = new Queue("spring_topic_queue", false);
+        Queue fanoutqueue = new Queue("spring_fanout_queue", false);
+
         rabbitAdmin.declareExchange(exchange);
+        rabbitAdmin.declareExchange(fanoutexchange);
+        rabbitAdmin.declareQueue(queue);
+        rabbitAdmin.declareQueue(fanoutqueue);
+        //rabbitAdmin.declareBinding(new Binding("spring_topic_queue", Binding.DestinationType.QUEUE, "spring_topic_exchange","spring.topic", new HashMap<>()));
+
+        rabbitAdmin.declareBinding(BindingBuilder.bind(queue).to(exchange).with("spring.topic").and(new HashMap<>()));
+
+        rabbitAdmin.declareBinding(BindingBuilder.bind(fanoutqueue).to(fanoutexchange));
+
+        rabbitAdmin.purgeQueue("spring_topic_queue",false);
         System.out.println(rabbitAdmin);
+    }
+
+    @Test
+    public void testSendMessage() throws Exception{
+        MessageProperties messageProperties = new MessageProperties();
     }
 }
